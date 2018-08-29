ce783413b7734192d9694b0996c7ccc10f9c5d0d
Tom Dennis <tdennis913@gmail.com>
2018-08-29 19:12:49 -0400

    restructured js to store current page state as object

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,142 +1,131 @@
 // Initial array of movies
 const topics = [
-    "Game of Thrones", 
-    "I Love You, Man", 
-    "Rick and Morty", 
-    "Spongebob", 
-    "Step Brothers", 
-    "South Park", 
-    "40 Year Old Virgin", 
-    "Borat", 
-    "Family Guy", 
-    "Parks and Recreation", 
-    "Talladega Nights", 
-    "Forgetting Sarah Marshall", 
+    "Game of Thrones",
+    "I Love You, Man",
+    "Rick and Morty",
+    "Spongebob",
+    "Step Brothers",
+    "South Park",
+    "40 Year Old Virgin",
+    "Borat",
+    "Family Guy",
+    "Parks and Recreation",
+    "Talladega Nights",
+    "Forgetting Sarah Marshall",
     "The Other Guys", "Elf"
 ];
 
-let currentTopic;
-let currentLimit;
-let currentOffset;
-let currentQueryURL;
+function GifPage() {
+    this.topics = topics;
+    this.currentTopic = "";
+    this.currentLimit = 10;
+    this.currentOffset = 0;
+    this.currentQueryURL = "";
+    //methods
+    this.buildQueryURL = (topic, offset) => {
+        this.currentTopic = topic;
 
-// Function for dumping the JSON content for each button into the div
-function displayGifInfo() {
+        let params = $.param({
+            api_key: "dc6zaTOxFJmzC",
+            q: topic,
+            limit: 10,
+            offset: offset
+        })
 
-    let topic = this.dataset.name;
-    let limit = 10;
-    let offset = 0;
-    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=dc6zaTOxFJmzC&limit=${limit}&offset=${offset}`;
+        return "https://api.giphy.com/v1/gifs/search?" + params;
+    };
+    this.buildGif = element => {
+        let gifContainer = $("<div>").attr("class", "gif-container");
+        let stillSrc = element.images.fixed_height_still.url;
+        let animatedSrc = element.images.fixed_height.url;
+        let img = $("<img>");
+        img.addClass("gif");
+        img.attr({
+            "src": stillSrc,
+            "data-still_url": stillSrc,
+            "data-animated_url": animatedSrc,
+            "data-currently_animated": "false"
+        });
+        let rating = $("<p>");
+        rating.attr("class", "rating");
+        rating.text(element.rating);
+        gifContainer.append(img); /* height="348px" width="480px"/>`);*/
+        gifContainer.append(rating);
 
-    currentTopic = topic;
-    currentLimit = limit;
-    currentOffset = offset;
-    currentQueryURL = queryURL;
+        return gifContainer;
+    };
+    // Function for dumping the JSON content for each button into the div
+    this.displayTopic = GiphyData => {
+        if (this.currentOffset === 0) {
+            this.clear();
+        }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(response => {
-        console.log(response);
         let topicContainer = $("<div>").attr("id", "topic-container");
-        for (const key in response.data) {
-            if (response.data.hasOwnProperty(key)) {
-                const element = response.data[key];
-                let gifContainer = $("<div>").attr("class", "gif-container");
-                let stillSrc = element.images.fixed_height_still.url;
-                let animatedSrc = element.images.fixed_height.url;
-                let img = $("<img>");
-                img.addClass("gif");
-                img.attr({
-                    "src": stillSrc, 
-                    "data-still_url": stillSrc, 
-                    "data-animated_url": animatedSrc, 
-                    "data-currently_animated": "false"
-                });
-                let rating = $("<p>");
-                rating.attr("class", "rating");
-                rating.text(element.rating);
-                gifContainer.append(img); /* height="348px" width="480px"/>`);*/
-                gifContainer.append(rating);
-                topicContainer.append(gifContainer);
+
+        for (const key in GiphyData.data) {
+            if (GiphyData.data.hasOwnProperty(key)) {
+                const element = GiphyData.data[key];
+                topicContainer.append(this.buildGif(element));
                 $("#gif-view").prepend(topicContainer);
             }
         }
-    });
+    };
+    this.renderButtons = () => {
+        $("#buttons-view").empty();
 
-}
-
-function addMoreGifs () {
-    currentOffset += 10;
-    currentQueryURL = `https://api.giphy.com/v1/gifs/search?q=${currentTopic}&api_key=dc6zaTOxFJmzC&limit=${currentLimit}&offset=${currentOffset}`
-
-    $.ajax({
-        url: currentQueryURL,
-        method: "GET"
-    }).then(response => {
-        console.log(response);
-        let topicContainer = $("<div>").attr("id", "topic-container");
-        for (const key in response.data) {
-            if (response.data.hasOwnProperty(key)) {
-                const element = response.data[key];
-                let gifContainer = $("<div>").attr("class", "gif-container");
-                let stillSrc = element.images.fixed_height_still.url;
-                let animatedSrc = element.images.fixed_height.url;
-                let img = $("<img>");
-                img.addClass("gif");
-                img.attr({
-                    "src": stillSrc, 
-                    "data-still_url": stillSrc, 
-                    "data-animated_url": animatedSrc, 
-                    "data-currently_animated": "false"
-                });
-                let rating = $("<p>");
-                rating.attr("class", "rating");
-                rating.text(element.rating);
-                gifContainer.append(img); /* height="348px" width="480px"/>`);*/
-                gifContainer.append(rating);
-                topicContainer.append(gifContainer);
-                $("#gif-view").prepend(topicContainer);
-            }
+        for (var i = 0; i < this.topics.length; i++) {
+            let a = $("<button>");
+            a.addClass("topic");
+            a.attr("data-name", this.topics[i]);
+            a.text(this.topics[i]);
+            $("#buttons-view").append(a);
         }
-    });
-}
-
-function renderButtons() {
-    $("#buttons-view").empty();
-
-    for (var i = 0; i < topics.length; i++) {
-        let a = $("<button>");
-        a.addClass("topic");
-        a.attr("data-name", topics[i]);
-        a.text(topics[i]);
-        $("#buttons-view").append(a);
+    };
+    this.animateGif = gif => {
+        let animatedSrc = gif.dataset.animated_url;
+        $(gif).attr("src", animatedSrc);
+        // $(this).attr("data-currently_animated", "true");
+    };
+    this.stopAnimatedGif = gif => {
+        let stillSrc = gif.dataset.still_url;
+        $(gif).attr("src", stillSrc);
+        // $(this).attr("data-currently_animated", "false");
+    };
+    this.clear = () => {
+        $("#gif-view").empty();
     }
 }
 
-function animateGif () {
-    let animatedSrc = this.dataset.animated_url;
-    $(this).attr("src", animatedSrc);
-    // $(this).attr("data-currently_animated", "true");
-}
-
-function stopAnimatedGif () {
-    let stillSrc = this.dataset.still_url;
-    $(this).attr("src", stillSrc);
-    // $(this).attr("data-currently_animated", "false");
-}
+let currentGifPage = new GifPage();
 
 $("#add-topic").on("click", event => {
     event.preventDefault();
 
     let topic = $("#topic-input").val().trim();
-    topics.push(topic);
-    renderButtons();
+    currentGifPage.topics.push(topic);
+    currentGifPage.renderButtons();
 });
 
-$(document).on("click", ".topic", displayGifInfo);
+// $(document).on("click", ".topic", displayGifInfo);
+$(document).on("click", ".topic", event => {
+    currentGifPage.currentOffset = 0;
+    let queryURL = currentGifPage.buildQueryURL(event.target.dataset.name, currentGifPage.currentOffset);
 
-$(document).on("click", "#add-more", addMoreGifs);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(currentGifPage.displayTopic);
+});
+
+$(document).on("click", "#add-more", () => {
+    currentGifPage.currentOffset += 10;
+    let queryURL = currentGifPage.buildQueryURL(currentGifPage.currentTopic, currentGifPage.currentOffset);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(currentGifPage.displayTopic);
+});
 
 // $(document).on("click", ".gif", function () {
 //     if (this.dataset.currently_animated === "false") {
@@ -146,8 +135,12 @@ $(document).on("click", "#add-more", addMoreGifs);
 //     }
 // });
 
-$(document).on("mouseover", ".gif", animateGif);
+$(document).on("mouseover", ".gif", event => {
+    currentGifPage.animateGif(event.currentTarget);
+});
 
-$(document).on("mouseout", ".gif", stopAnimatedGif);
+$(document).on("mouseout", ".gif", event => {
+    currentGifPage.stopAnimatedGif(event.currentTarget);
+});
 
-renderButtons();
+currentGifPage.renderButtons();
